fee51fec1ff16f014256f25f1800c71f35b62967
AngelosKl <rtfon2@gmail.com>
2024-09-23 20:39:58 +0300

    css responsive changes kalivas

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Listen on all interfaces
+    port: 3000,      // Change this if Render expects a different port
+    strictPort: true // Fail if the port is already in use
+  }
+});
